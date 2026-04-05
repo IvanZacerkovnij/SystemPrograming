755fb6ef1cfb6ddb841139ebc1ac42dc3a4ad30d
IvanZacerkovnij <ivan.zacerkovnij@gmail.com>
2026-04-05 16:15:29 +0300

    homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework3;
+
+class Program
+{
+    private static readonly int _printCount = 20;
+    
+    static void PrintLow()
+    {
+        for (int i = 0; i < _printCount; i++)
+        {
+            Console.WriteLine("Low");
+            Thread.Sleep(100);
+        }
+    }
+
+    static void PrintNormal()
+    {
+        for (int i = 0; i < _printCount; i++)
+        {
+            Console.WriteLine("Normal");
+            Thread.Sleep(100);
+        }
+    }
+
+    static void PrintHigh()
+    {
+        for (int i = 0; i < _printCount; i++)
+        {
+            Console.WriteLine("High");
+            Thread.Sleep(100);
+        }
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        Thread threadLow = new Thread(PrintLow);
+        Thread threadNormal = new Thread(PrintNormal);
+        Thread threadHigh = new Thread(PrintHigh);
+        
+        threadLow.Priority = ThreadPriority.Lowest;
+        threadNormal.Priority = ThreadPriority.Normal;
+        threadHigh.Priority = ThreadPriority.Highest;
+        
+        threadLow.Start();
+        threadNormal.Start();
+        threadHigh.Start();
+    }
+}
